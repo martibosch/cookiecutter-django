f5435c587cbfcd2c6c14eb1704307170feacb8dd
Martí Bosch <marti.bosch@protonmail.com>
2022-05-10 17:02:01 +0200

    fix serve-staging script: production.staging.yml -> staging.yml

--- {{cookiecutter.project_slug}}/terraform/serve-staging.sh
+++ {{cookiecutter.project_slug}}/terraform/serve-staging.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/app
-docker-compose -f production.yml -f production.staging.yml run --rm django python manage.py migrate
-docker-compose -f production.yml -f production.staging.yml up --build -d
+docker-compose -f production.yml -f staging.yml run --rm django python manage.py migrate
+docker-compose -f production.yml -f staging.yml up --build -d
